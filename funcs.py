ffaefa88b1eed791d15c51fed7dbdfe6051159ea
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-13 15:55:06 +0400

    add subtraction_number into funcs.py

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def sum_number(a, b):
     """ Принимает 2 числа и возвращает их сумму. """
     return a + b
+
+
+def subtraction_number(a, b):
+    """ Принимает 2 числа и возвращает их разность. """
+    return a - b
